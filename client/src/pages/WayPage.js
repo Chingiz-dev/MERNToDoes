35455c945e8a8629bfb6f168f20d8eb188e0ac05
Chingiz Shigayev <shigayevphone@gmail.com>
2021-03-09 15:34:04 +0400

    (feat):WayPage.js, couple links were added

--- client/src/pages/WayPage.js
+++ client/src/pages/WayPage.js
@@ -1,14 +1,40 @@
+import st from './TodosPage.module.css'
+
 export const WayPage = () => {
   return (
-    <div>
-      <h2>How I made it page</h2>
-      <iframe
-        src="https://www.youtube.com/embed/ivDjWYcKDZI"
-        title="myWay"
-        frameBorder="0"
-        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    <div className={st.container}>
+      <div>
+        <h2>Streamline to become a FullStackDeveloper</h2>
+        <iframe
+          src="https://www.youtube.com/embed/181VnUUJq-Y"
+          title="Guideline"
+          frameBorder="0"
+          // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div>
+        <h2>MERN concept</h2>
+        <iframe
+          src="https://www.youtube.com/embed/ivDjWYcKDZI"
+          title="myWay"
+          frameBorder="0"
+          // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div>
+        <h2>Webpack freecodcamp.org concept</h2>
+        <iframe
+          src="https://www.youtube.com/embed/MpGLUVbqoYQ"
+          title="myWay"
+          frameBorder="0"
+          // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
